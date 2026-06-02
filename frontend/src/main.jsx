7d57f63c6b784910860437082d04700494e29420
Zatta <zhafranediting23@gmail.com>
2026-06-02 20:55:27 +0700

    Update 13.0

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import axios from 'axios'
 import App from './App.jsx'
 import './index.css'
+
+axios.defaults.baseURL = 'https://website-cek-nisn-production.up.railway.app'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
